815eed1a31b7df640a66d6ddd5fb01158087055e
Mikhail Damakalshchikov <93596198+damakalshchikov@users.noreply.github.com>
2026-07-19 22:34:23 +0300

    Метрики уровня метода: @measure на get_or_404

--- app/resources/common.py
+++ app/resources/common.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 
+from flask_service_metrics import measure
 from flask_smorest import abort
 
 from app.extensions import Base, db
 
 
+@measure
 def get_or_404[ModelT: Base](model: type[ModelT], id_: UUID, message: str) -> ModelT:
     obj = db.session.get(model, id_)
     if obj is None:
